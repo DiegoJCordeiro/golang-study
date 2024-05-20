7a7432eba361d12ba54f093e6f279d62726e7d60
DiegoJCordeiro <diegocordeiro.contatos@gmail.com>
2024-05-20 14:34:20 -0300

    Feature: Added interfaces example in interfaces.go

--- chapter1/interfaces.go
+++ chapter1/interfaces.go
@@ -4,9 +4,33 @@ import (
 	"fmt"
 )
 
+type PhysicalPerson struct {
+	fit string
+}
+
+type TypeFit interface {
+	changeFit(fitType string)
+	getFit() string
+}
+
+func (physicalPerson *PhysicalPerson) changeFit(fitType string) {
+	physicalPerson.fit = fitType
+}
+func (physicalPerson *PhysicalPerson) getFit() string {
+	return physicalPerson.fit
+}
+
+func showFit(typeFit TypeFit) {
+	fmt.Printf("Fit Type is: %v\n", typeFit.getFit())
+}
+
 func lesson9() {
 
 	fmt.Println("Lesson 9 - Interfaces")
-
+	var physicalPerson = PhysicalPerson{
+		fit: "Ecto morph",
+	}
+	physicalPerson.changeFit("Mesomorph")
+	showFit(&physicalPerson)
 	fmt.Println("---")
 }
