f53a8c9f8aa4f27013afa5738107931b30bc85cf
DiegoJCordeiro <diegocordeiro.contatos@gmail.com>
2024-05-24 10:27:13 -0300

    Feature: Added import of external modules.

--- chapter1/packages/pkg.go
+++ chapter1/packages/pkg.go
@@ -1,7 +1,13 @@
 package packages
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 func ShowPackage() {
+
+	newUUID, err := uuid.NewUUID()
 	fmt.Printf("Calling Internal Package.\n")
+	fmt.Printf("UUID generated: %+v, Error: %+v.\n", newUUID, err)
 }
