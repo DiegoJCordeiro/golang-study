c4e594fac494111de4c772dca97ea85d7a162335
DiegoJCordeiro <diegocordeiros.contatos@gmail.com>
2024-05-28 13:55:25 -0300

    Feature: Added lesson about manipulate files.

---
+++ chapter2/files.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	errWrite  error
+	errClose  error
+	errFlush  error
+	errOpen   error
+	errCreate error
+)
+
+func createFile() {
+
+	file, errCreate := os.Create("./file_test.txt")
+
+	if errCreate != nil {
+		panic(errCreate)
+	}
+
+	errClose := file.Close()
+
+	if errClose != nil {
+		panic(errClose)
+	}
+}
+
+func writeFile() {
+
+	errWrite := os.WriteFile("./file_test.txt", []byte("- First Line here."), 777)
+
+	if errWrite != nil {
+		panic(errWrite)
+	}
+}
+
+func writeLineToLineFile() {
+	file, errOpen := os.OpenFile("./file_test.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+	if errOpen != nil {
+		panic(errOpen)
+	}
+
+	writer := bufio.NewWriter(file)
+
+	_, errWrite = writer.WriteString("\n")
+	_, errWrite = writer.Write([]byte("- Second Line."))
+	_, errWrite = writer.WriteString("\n")
+	_, errWrite = writer.Write([]byte("- Third Line."))
+	_, errWrite = writer.WriteString("\n")
+	_, errWrite = writer.Write([]byte("- Fourth Line."))
+	errFlush := writer.Flush()
+
+	errClose := file.Close()
+
+	if errClose != nil {
+		panic(errClose)
+	} else if errFlush != nil {
+		panic(errFlush)
+	} else if errWrite != nil {
+		panic(errWrite)
+	}
+}
+
+func readFile() {
+
+	file, errOpen := os.ReadFile("./file_test.txt")
+
+	if errOpen != nil {
+		panic(errOpen)
+	}
+
+	fmt.Printf("Content file: \n%s \n", string(file))
+}
+
+func readLineToLineFile() {
+
+	file, errOpen := os.Open("./file_test.txt")
+
+	if errOpen != nil {
+		panic(errOpen)
+	}
+
+	reader := bufio.NewReader(file)
+
+	fmt.Printf("Content file: \n")
+	for {
+		line, _, errReader := reader.ReadLine()
+
+		if errReader != nil {
+			fmt.Printf("End Content.\n")
+			break
+		}
+
+		fmt.Printf("%s\n", string(line))
+	}
+}
+
+func Lesson1() {
+
+	fmt.Println("Lesson 1 - Manipulate Files")
+
+	createFile()
+	writeFile()
+	readFile()
+	writeLineToLineFile()
+	readLineToLineFile()
+
+	fmt.Println("---")
+}
--- chapter2/main.go
+++ chapter2/main.go
@@ -1,8 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	fmt.Printf("Hello World - Chapter 2\n")
+	Lesson1()
 }
